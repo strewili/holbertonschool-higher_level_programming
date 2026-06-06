41b9fc213e7d4e5947279bb6006351c61a7babab
strewili <shouqaaa123@gmail.com>
2026-06-06 11:24:36 +0000

    S

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    count = len(argv) - 1
+
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+    else:
+        print("{} arguments:".format(count))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
