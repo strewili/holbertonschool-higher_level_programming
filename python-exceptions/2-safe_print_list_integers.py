5b6b9aac426a380e784b2d66445d4631cbff8f79
strewili <shouqaaa123@gmail.com>
2026-06-13 13:43:41 +0300

    Fix safe print list integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
         except IndexError:
-            break
+            continue
     print()
     return count
