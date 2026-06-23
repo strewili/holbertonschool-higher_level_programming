d53ca15cfd5490f9b836492b5a7e8d10a67fa77e
strewili <shouqaaa123@gmail.com>
2026-06-23 11:53:57 +0300

    Rename function to check for inherited instances

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a class and inherited class-checking function."""
+"""Defines an inherited class-checking function."""
 
 
-def is_kind_of_class(obj, a_class):
-    """Returns True if object is an instance or inherited instance."""
-    return isinstance(obj, a_class)
+def inherits_from(obj, a_class):
+    """Returns True if object is an inherited instance (subclass) only."""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
