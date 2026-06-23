3f1c3657a2571a6f9793890306816aad0f99ea1f
strewili <shouqaaa123@gmail.com>
2026-06-23 12:05:25 +0300

    Fix condition to check subclass inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Returns True if object is an inherited instance (subclass) only."""
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
