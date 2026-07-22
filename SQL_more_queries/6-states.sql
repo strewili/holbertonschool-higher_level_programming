afa166f8dabf4203a79274791ae02e4017f30bfa
strewili <shouqaaa123@gmail.com>
2026-07-22 16:20:18 +0300

    Add comment for user creation in 6-states.sql
    Added a comment for user creation in the SQL script.

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
+-- Creates the user user_0d_1 with all privileges.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
